3ace00638d453ae8139aa4b70bae9e6d91dfb3f1
luisbarahona08 <luis.barahona.paca@udla.edu.ec>
2026-03-31 12:32:15 -0500

    Update Barahona_Luis_ejercicio_S2.c
    codigo final con el ultimo for

--- Barahona_Luis_ejercicio_S2.c
+++ Barahona_Luis_ejercicio_S2.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 int main(){
-int n, i, A[i};
+int n, i, A[50];
+      
     printf("Ingrese la cantidad de elementos que quiere ingresar para el vector para el vector: ");
     scanf("%d", &n);
-    for(i=1; i<n ; i++){
-        printf("Ingrese el numero %d: ", i);
+    for(i=0; i<n ; i++){
+        printf("%d ", i);
         scanf("%d", &A[i]);
     }
+    for(i=0; i<n ; i++){
+    printf("Los numeros ingresados son: %d\n", A[i]); 
+    }
+    return 0;
 }
