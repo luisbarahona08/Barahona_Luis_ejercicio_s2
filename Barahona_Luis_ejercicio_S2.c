03fc91edb42ebf280b1b33a483681e14a6277dba
luisbarahona08 <luis.barahona.paca@udla.edu.ec>
2026-03-31 12:12:49 -0500

    Update Barahona_Luis_ejercicio_S2.c
    primer para

--- Barahona_Luis_ejercicio_S2.c
+++ Barahona_Luis_ejercicio_S2.c
@@ -3,5 +3,8 @@ int main(){
 int n, i, A[i};
     printf("Ingrese la cantidad de elementos que quiere ingresar para el vector para el vector: ");
     scanf("%d", &n);
-
+    for(i=1; i<n ; i++){
+        printf("Ingrese el numero %d: ", i);
+        scanf("%d", &A[i]);
+    }
 }
